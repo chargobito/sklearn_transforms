4e921d25568ddd8aa75a3f18560be61c5d191e61
chargobito <64793672+chargobito@users.noreply.github.com>
2020-08-21 19:31:55 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,25 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+    
+class Nota(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        return
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        data = X.copy()
+        
+        for index, row in data.iterrows():
+            if(row['NOTA_EM'] > 10):
+                data.loc[data.index == index, 'NOTA_EM'] = 10
+            if(row['NOTA_DE'] > 10):
+                data.loc[data.index == index, 'NOTA_DE'] = 10
+            if(row['NOTA_MF'] > 10):
+                data.loc[data.index == index, 'NOTA_MF'] = 10
+            if(row['NOTA_GO'] > 10):
+                data.loc[data.index == index, 'NOTA_GO'] = 10
+                
+        return data
